d16f5a57c2d8d773bb847a181c1134622de5edf7
romolonobre <romoloferreira@icloud.com>
2024-08-14 10:55:40 +0100

    remove navigation logic from splashscreen

--- lib/app/spash_screen.dart
+++ lib/app/spash_screen.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import '_commons/app_services/cache.dart';
 import '_commons/app_services/utils.dart';
 import '_commons/imovie_ui/iui_text.dart';
 import '_commons/push_notifications/push_notifications.dart';
-import '_commons/user/entities/app_user.dart';
+import '_commons/user_status/verify_user_status.dart';
 import 'authentication/interactor/login_controller.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -23,23 +22,8 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     final user = controller.getUser();
-    handleNavigation(user);
-    Future(() async => await PushNotifications.initialize());
-  }
-
-  void handleNavigation(AppUser? user) {
-    bool isLogged = controller.isLogged();
-
-    final String navigatoPath = isLogged ? "home" : "login";
-    bool isBiometricsAuthEnabled = Cache().isBiometricsEnabled() ?? false;
-
-    if (!isLogged) {
-      Modular.to.navigate('/login/');
-    } else if (!isBiometricsAuthEnabled) {
-      Future.delayed(const Duration(seconds: 2)).then((_) => Modular.to.navigate('/$navigatoPath/'));
-    } else {
-      Modular.to.navigate('/biometrics/');
-    }
+    VerifyUserStatus(user: user, controller: controller);
+    Future(() => PushNotifications.initialize());
   }
 
   @override
