6c3a1bbe4cb03eeb604c9ad1d30012ecf48814b1
romolonobre <romoloferreira@icloud.com>
2024-10-15 16:49:31 +0100

    WIP imovie package

---
+++ lib/packages/imovie_ui/lib/src/imovie_ui.dart
@@ -0,0 +1,7 @@
+/// {@template imovie_ui}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class ImovieUi {
+  /// {@macro imovie_ui}
+  const ImovieUi();
+}
---
+++ lib/packages/imovie_ui/test/src/imovie_ui_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:imovie_ui/imovie_ui.dart';
+
+void main() {
+  group('ImovieUi', () {
+    test('can be instantiated', () {
+      expect(ImovieUi(), isNotNull);
+    });
+  });
+}
